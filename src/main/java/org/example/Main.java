7672406c8a0bd226603b2f3b0bbe63c0bc4586de
StepanTeshka <leluhinstepan@gmail.com>
2023-09-05 09:58:41 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Metodi me = new Metodi();
+        me.printThreeWords();
+        me.сheckSumSign(1, 3);
+        me.printColor(110);
+        me.compareNumbers(10,11);
+    }
+}
